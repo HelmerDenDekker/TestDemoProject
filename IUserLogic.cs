68e43548c4524be7eebe5882078db7e14eb90c92
Helmer den Dekker <dekker@w-e.nl>
2020-11-19 16:08:19 +0100

    Add project files.

---
+++ IUserLogic.cs
@@ -0,0 +1,10 @@
+﻿using Model;
+
+namespace TestDemoProject
+{
+    public interface IUserLogic
+    {
+
+        public UserInfoDto GetUserInfo(int userId);
+    }
+}
---
+++ UserLogic.cs
@@ -0,0 +1,55 @@
+﻿using Model.DalRepositories;
+
+namespace TestDemoProject
+{
+    public class UserLogic : IUserLogic
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserLogic(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        /// <summary>
+        /// Query style
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public UserInfoDto GetUserInfo(int userId)
+        {
+            var user= _userRepository.FindById(userId);
+            var userInfoDto = new UserInfoDto
+            {
+                UserId = user.Id,
+                UserName = user.UserName
+            };
+            return userInfoDto;
+        }
+
+        /// <summary>
+        /// Command style
+        /// </summary>
+        /// <param name="newUser"></param>
+        public void AddNewUser(NewUserDto newUser)
+        {
+            var user = new User { UserName = newUser.UserName };
+            _userRepository.AddUser(user);
+        }
+
+        /// <summary>
+        /// Mixing command style and query style, since we want to return the new data to the FE
+        /// </summary>
+        /// <param name="userInfo"></param>
+        /// <returns></returns>
+        public UserInfoDto UpdateUserInfo(UserInfoDto userInfo)
+        {
+            var user = new User {Id = userInfo.UserId, UserName = userInfo.UserName};
+            _userRepository.UpdateUser(user);
+            return GetUserInfo(userInfo.UserId);
+        }
+
+    }
+
+
+}
